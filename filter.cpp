11a851779ebccac137fa4c39733b96eadeed0449
Shaun McGuigan <Shaun@Goldenrod.local>
2020-06-03 13:20:08 -0700

    Moved constructor for filter to implementation file.

--- filter.cpp
+++ filter.cpp
@@ -2,27 +2,46 @@
 #include "filter.h"
 
 
-void filter::produce_output() {
+SC_HAS_PROCESS(filter);
+filter::filter(sc_module_name n) : sc_module(n)
+{
+    SC_METHOD(produce_output);
+    sensitive << s2 << s4 << s5;
+    
+    SC_METHOD(handle_input);
+    sensitive << x << s2 << s4 << s5;
+    
+    SC_CTHREAD(clock_thread, clock.pos());
+    reset_signal_is(reset, true);
+}
+
+
+void filter::produce_output()
+{
     y->write((s2.read() * 0.24) + (s4.read() * 0.2) + (s5.read() * 0.25));
 }
 
 
-void filter::handle_input() {
+void filter::handle_input()
+{
     s1.write(x->read() + (s2.read() * 0.4));
     s3.write(x->read() + (s4.read() * -0.8) + (s5.read() * -0.5));
 }
 
 
-void filter::clock_thread() {
+void filter::clock_thread()
+{
     // Reset actions
-    if (reset) {
+    if (reset)
+    {
         s2.write(0.0);
         s4.write(0.0);
         s5.write(0.0);
         wait();
     }
     
-    while (true) {
+    while (true)
+    {
         // Read current inputs
         s2.write(s1.read());
         s4.write(s3.read());
--- filter.h
+++ filter.h
@@ -1,7 +1,8 @@
 #include "systemc.h"
 
 
-SC_MODULE(filter) {
+SC_MODULE(filter)
+{
     // Ports
     sc_in<float> x;
     sc_in<bool> reset;
@@ -11,20 +12,11 @@ SC_MODULE(filter) {
     // Internal signals
     sc_signal<float> s1, s2, s3, s4, s5;
     
+    // Constructor
+    filter(sc_module_name n);
+    
     // Processes
     void produce_output();
     void handle_input();
     void clock_thread();
-    
-    // Constructor
-    SC_CTOR(filter) {
-        SC_METHOD(produce_output);
-        sensitive << s2 << s4 << s5;
-        
-        SC_METHOD(handle_input);
-        sensitive << x << s2 << s4 << s5;
-        
-        SC_CTHREAD(clock_thread, clock.pos());
-        reset_signal_is(reset, true);
-    }
 };
